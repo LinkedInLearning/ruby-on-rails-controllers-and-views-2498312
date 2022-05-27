b1474827603c298688913a86c7e9d7018b502eb3
Kevin Skoglund <kevin@novafabrica.com>
2022-05-27 18:11:40 -0400

    Finish movie 04_06

--- task_manager/app/helpers/application_helper.rb
+++ task_manager/app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def error_messages_for(object)
+    render(partial: 'layouts/error_messages', locals: {object: object})
+  end
+
+  def status_tag(boolean, options={})
+    options[:true_text] ||= ''
+    options[:false_text] ||= ''
+
+    if boolean
+      content_tag(:span, options[:true_text], class: 'status true')
+    else
+      content_tag(:span, options[:false_text], class: 'status false')
+    end
+  end
+
 end
